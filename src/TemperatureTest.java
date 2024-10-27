7b0bfd0eb20eaf2ae083f7490775edb2566fabeb
Serhii Pidonenko <gspsnake+gh@gmail.com>
2024-10-27 13:04:47 +0200

    Complete the Temperature conversion assignment; add more tests

--- src/TemperatureTest.java
+++ src/TemperatureTest.java
@@ -14,8 +14,10 @@ public class TemperatureTest {
         return new Object[][]{
                 {212.0, 100.0},
                 {32.0, 0.0},
-                {-15, -26.11111111111111}
+                {-15, -26.11111111111111},
                 // TODO add 2 more test data here
+                {0, -17.777777777777777777},
+                {100, 37.77777777777777777},
         };
     }
 
@@ -28,8 +30,10 @@ public class TemperatureTest {
         return new Object[][]{
                 {100.0, 212.0},
                 {0.0, 32.0},
-                {-26.11111111111111, -15}
+                {-26.11111111111111, -15},
                 // TODO add 2 more test data here
+                {36.65, 97.97},
+                {193, 379.4},
         };
     }
 
