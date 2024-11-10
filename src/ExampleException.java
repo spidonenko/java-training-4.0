9f544ea1c13ddf6dd6b18ee95a55dc1a168e92a1
Serhii Pidonenko <gspsnake+gh@gmail.com>
2024-11-10 15:30:17 +0200

    Exception assignment: implement `rectangleArea` and test methods, add 2 more tests for each category.

--- src/ExampleException.java
+++ src/ExampleException.java
@@ -2,6 +2,11 @@ public class ExampleException {
 
     public static int rectangleArea(int a, int b) {
         // TODO put your code here
-        return 0;
+        // I would rather check the parameters is strictly greater than 0
+        // but it looks like it is possible to have 0-area rectangle conceptually
+        if (a < 0 || b < 0) {
+            throw new IllegalArgumentException(String.format("(a=%d;b=%d) input value is below zero!", a, b));
+        }
+        return a*b;
     }
 }
--- src/ExampleExceptionTest.java
+++ src/ExampleExceptionTest.java
@@ -1,8 +1,7 @@
-import static org.testng.Assert.assertEquals;
-import static org.testng.Assert.fail;
-
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
 
 public class ExampleExceptionTest {
 
@@ -15,8 +14,10 @@ public class ExampleExceptionTest {
     public static Object[][] data() {
         return new Object[][]{
                 {2, 2, 4},
-                {2, 3, 6}
+                {2, 3, 6},
                 // TODO add 2 more test data here
+                {5, 4, 20},
+                {1, 0, 0}  // valid corner case
         };
     }
 
@@ -29,19 +30,36 @@ public class ExampleExceptionTest {
     public static Object[][] negativeData() {
         return new Object[][]{
                 {-2, 2},
-                {2, -2}
+                {2, -2},
                 // TODO add 2 more test data here
+                {-3, -5},
+                {-2, 0}
         };
     }
 
     @Test(dataProvider = "data")
     public void testRectangleArea(int a, int b, int c) {
         // TODO put your code here
+        assertEquals(ExampleException.rectangleArea(a, b), c, "Wrong calculated rectangle area");
     }
 
 
     @Test(dataProvider = "negativeData")
     public void testRectangleAreaNegative(int a, int b) {
         // TODO put your code here
+        //// Solution #1 ////
+        // using test framework method
+        var e = expectThrows(IllegalArgumentException.class, () -> ExampleException.rectangleArea(a, b));
+        assertTrue(e.getMessage().contains("input value is below zero!"), "Exception message is wrong: " + e.getMessage());
+
+        //// Solution #2 ////
+        // manually catch expected exception
+//        try {
+//            var area = ExampleException.rectangleArea(a, b);
+//        } catch (IllegalArgumentException e) {
+//            assertTrue(e.getMessage().contains("input value is below zero!"), "Exception message is wrong");
+//            return;
+//        }
+//        fail("expected IllegalArgumentException is not detected");
     }
 }
