a1a287111ff454619797e1fb93e5c1de53d69151
Serhii Pidonenko <gspsnake+gh@gmail.com>
2024-12-16 09:58:30 +0200

    Appium assignment: complete the Page objects and steps (for iOS and Android)

--- src/main/java/com/example/appium/PageView.java
+++ src/main/java/com/example/appium/PageView.java
@@ -1,13 +1,29 @@
 package com.example.appium;
 
 import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.pagefactory.AndroidFindBy;
 import io.appium.java_client.pagefactory.AppiumFieldDecorator;
+import io.appium.java_client.pagefactory.iOSXCUITFindBy;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
 
 public class PageView {
     AppiumDriver driver;
 
     // TODO define textField MobileElement using @FindBy kind of annotations for iOS and Android
+    @AndroidFindBy(id = "io.appium.android.apis:id/edit")
+    @iOSXCUITFindBy(iOSNsPredicate =  "name = 'IntegerA'")
+    WebElement textFieldA;
+
+    @AndroidFindBy(id = "io.appium.android.apis:id/edit1")
+    @iOSXCUITFindBy(iOSNsPredicate =  "name = 'IntegerB'")
+    WebElement textFieldB;
+
+    @iOSXCUITFindBy(iOSNsPredicate =  "name = 'Compute Sum'")
+    WebElement sumBtn;
+
+    @iOSXCUITFindBy(iOSNsPredicate =  "name = 'Answer'")
+    WebElement answerLbl;
 
     public PageView(AppiumDriver driver) {
         this.driver = driver;
@@ -16,11 +32,32 @@ public class PageView {
 
     public String getTextField() {
         // TODO return text from the textField element
-        return "";
+        return textFieldA.getText();
     }
 
     public PageView setTextField(String text) {
         // TODO set text to the textField element
+        textFieldA.clear();
+        textFieldA.sendKeys(text);
         return this;
+    }
+
+    public String getTextFieldB() {
+        return textFieldB.getText();
+    }
+
+    public PageView setTextFieldB(String text) {
+        textFieldB.clear();
+        textFieldB.sendKeys(text);
+        return this;
+    }
+
+    public PageView clickSumBtn() {
+        sumBtn.click();
+        return this;
+    }
+
+    public String getAnswerLbl() {
+        return answerLbl.getText();
     }
 }
--- src/test/java/com/example/appium/SampleAppTest.java
+++ src/test/java/com/example/appium/SampleAppTest.java
@@ -8,6 +8,7 @@ import io.appium.java_client.ios.IOSDriver;
 import io.appium.java_client.ios.options.XCUITestOptions;
 import io.appium.java_client.service.local.AppiumDriverLocalService;
 import io.appium.java_client.service.local.AppiumServiceBuilder;
+import org.testng.SkipException;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -15,10 +16,12 @@ import org.testng.annotations.Test;
 import java.nio.file.Paths;
 
 import static java.lang.System.getenv;
+import static org.testng.Assert.assertEquals;
 
 public class SampleAppTest {
     private AppiumDriverLocalService server;
     private AppiumDriver driver;
+    private PageView view;
 
     @BeforeClass
     private void setUp() {
@@ -29,7 +32,8 @@ public class SampleAppTest {
         if (platform.equals("ANDROID")) {
             var options = new UiAutomator2Options()
                     .setPlatformName("Android")
-                    .setDeviceName("PUT_YOUR_DEVICE_NAME_HERE")
+                    .setDeviceName("80HBDPE223EB")  // using real device
+                    .setAppActivity(".view.TextFields")
                     .setApp(Paths.get(path).resolve("ApiDemos-debug.apk").toString());
 
             server = AppiumDriverLocalService.buildService(new AppiumServiceBuilder().usingAnyFreePort());
@@ -40,22 +44,36 @@ public class SampleAppTest {
         } else {
             var options = new XCUITestOptions()
                     .setPlatformName("iOS")
-                    .setPlatformVersion("PUT_YOUR_XCODE_VERSION_HERE")
+                    .setPlatformVersion("18.2")
                     .setAutomationName("XCuiTest")
-                    .setDeviceName("PUT_YOUR_DEVICE_NAME_HERE")
+                    .setDeviceName("iPhone SE (3rd generation)")
                     .setApp(Paths.get(path).resolve("TestApp.app.zip").toString());
 
             server = AppiumDriverLocalService.buildService(new AppiumServiceBuilder().usingAnyFreePort());
             server.start();
             driver = new IOSDriver(server, options);
         }
+        view = new PageView(driver);
     }
 
     @Test
     public void textFieldTest() {
         // TODO initialise PageView and set "text" to its textField
+        String textToVerify = "text";
+        view.setTextField(textToVerify);
 
         // TODO assert that textField equals to "text"
+        assertEquals(view.getTextField(), textToVerify, "Wrong text!");
+    }
+
+    @Test
+    public void intSumTest() {
+        if (!getenv("APPIUM_DRIVER").equalsIgnoreCase("IOS")){
+            throw new SkipException("This test is available only for iOS app");
+        }
+        view.setTextField("1").setTextFieldB("2").clickSumBtn();
+
+        assertEquals(view.getAnswerLbl(), "3", "Wrong sum!");
     }
 
     @AfterClass
